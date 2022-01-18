5768f33440fe36c3f7e540ae8d9969547091bd11
Leonardo Wainer <leowainer.lw@gmail.com>
2022-01-18 11:33:52 -0300

    Replica de Codigo, com alteracoes para gerar um orçamento

--- index.js
+++ index.js
@@ -33,25 +33,15 @@ async function createPdf(input, output) {
 
     const form = pdfDoc.getForm();
     
-    form.getTextField('n_contrato').setText(`${process.env.n_contrato}`)
-    form.getTextField('resp_venda').setText(`${process.env.resp_venda}`)
-    form.getTextField('loja').setText(`${process.env.loja}`)
-    form.getTextField('data_contrato').setText(`${process.env.data_contrato}`)
-    form.getTextField('tipo_contrato').setText(`${process.env.tipo_contrato}`)
-    form.getTextField('cpf_cnpj').setText(`${process.env.cpf_cnpj}`)
-    form.getTextField('rg_inscEst').setText(`${process.env.rg_inscEst}`)
-    form.getTextField('data_nasc').setText(`${process.env.data_nasc}`)
-    form.getTextField('endereco_atual').setText(`${process.env.endereco_atual}`)
-    form.getTextField('bairro').setText(`${process.env.bairro}`)
-    form.getTextField('cidade').setText(`${process.env.cidade}`)
-    form.getTextField('uf').setText(`${process.env.uf}`)
-    form.getTextField('cep').setText(`${process.env.cep}`)
-    form.getTextField('telefone').setText(`${ process.env.telefone}`)
-    form.getTextField('email').setText(`${process.env.email}`)
-    form.getTextField('cliente_contratante').setText(`${process.env.cliente_contratante}`)
-    form.getTextField('ambiente1').setText(lorem.generateParagraphs(8))
-    form.getTextField('ambiente2').setText(lorem.generateParagraphs(3))
-
+    form.getTextField('Nome').setText(`${process.env.Nome}`)
+    form.getTextField('Email').setText(`${process.env.Email}`)
+    form.getTextField('End_Entrega').setText(`${process.env.End_Entrega}`)
+    form.getTextField('Vendedor').setText(`${process.env.Vendedor}`)
+    form.getTextField('Gerente').setText(`${process.env.Gerente}`)
+    form.getTextField('Data').setText(`${process.env.Data}`)
+    form.getTextField('Data_Validade').setText(`${process.env.data_validade}`)
+    form.getTextField('n_orcamento').setText(`${process.env.n_orcamento}`)
+    form.getTextField('descricao').setText(lorem.generateParagraphs(5))
     
     const pdfBytes = await pdfDoc.save();
 
@@ -64,4 +54,4 @@ async function createPdf(input, output) {
 
 }
 
-createPdf(`${process.env.BASE_FILE}`, `contratoId${process.env.n_contrato}.pdf`)
+createPdf(`${process.env.BASE_FILE}`, `contratoId${process.env.n_orcamento}.pdf`)
