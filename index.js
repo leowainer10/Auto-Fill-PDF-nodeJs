e66154e2261d1bde87e0d284d4fb94dc0e966ff5
Leonardo Wainer <leowainer.lw@gmail.com>
2022-03-10 10:54:03 -0300

    Att

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const { PDFDocument } = require('pdf-lib');
-const { readFile , writeFile } = require('fs/promises');
-const LoremIpsum = require('lorem-ipsum').LoremIpsum;
-const dotenv = require('dotenv');
+const { PDFDocument } = require("pdf-lib");
+const { readFile, writeFile } = require("fs/promises");
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const dotenv = require("dotenv");
 dotenv.config();
-async function createPdf(input, output) {
+const defaultFields = [
+  'n_contrato',
+  'resp_venda',
+  'loja',
+  'data_contrato',
+  'tipo_contrato',
+  'cpf_cnpj',
+  'rg_inscEst',
+  'data_nasc',
+  'endereco_atual',
+  'bairro',
+  'cidade',
+  'uf',
+  'cep',
+  'telefone',
+  'email',
+  'cliente_contratante',
+]
 
-    try{
+async function createPdf(input, output) {
+  try {
     const pdfDoc = await PDFDocument.load(await readFile(input));
+    // pdf
 
     //Printa no terminal todos os nomes dos campos do Formulario
-    
-    // const fieldNames = pdfDoc.getForm().getFields().map(f => f.getName());
 
+    // const fieldNames = pdfDoc.getForm().getFields().map(f => f.getName());
     // fieldNames.forEach(element =>{
     //     console.log(element);
     // });
@@ -19,49 +37,39 @@ async function createPdf(input, output) {
     //Lorem Ipsum para preencher campos MultiLinhas
 
     const lorem = new LoremIpsum({
-        sentencesPerParagraph: {
-          max: 8,
-          min: 4
-        },
-        wordsPerSentence: {
-          max: 16,
-          min: 4
-        }
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
     });
 
     //Atribui valor para os FormFields
 
     const form = pdfDoc.getForm();
-    
-    form.getTextField('n_contrato').setText(`${process.env.n_contrato}`)
-    form.getTextField('resp_venda').setText(`${process.env.resp_venda}`)
-    form.getTextField('loja').setText(`${process.env.loja}`)
-    form.getTextField('data_contrato').setText(`${process.env.data_contrato}`)
-    form.getTextField('tipo_contrato').setText(`${process.env.tipo_contrato}`)
-    form.getTextField('cpf_cnpj').setText(`${process.env.cpf_cnpj}`)
-    form.getTextField('rg_inscEst').setText(`${process.env.rg_inscEst}`)
-    form.getTextField('data_nasc').setText(`${process.env.data_nasc}`)
-    form.getTextField('endereco_atual').setText(`${process.env.endereco_atual}`)
-    form.getTextField('bairro').setText(`${process.env.bairro}`)
-    form.getTextField('cidade').setText(`${process.env.cidade}`)
-    form.getTextField('uf').setText(`${process.env.uf}`)
-    form.getTextField('cep').setText(`${process.env.cep}`)
-    form.getTextField('telefone').setText(`${ process.env.telefone}`)
-    form.getTextField('email').setText(`${process.env.email}`)
-    form.getTextField('cliente_contratante').setText(`${process.env.cliente_contratante}`)
-    form.getTextField('ambiente1').setText(lorem.generateParagraphs(8))
-    form.getTextField('ambiente2').setText(lorem.generateParagraphs(3))
 
-    
+     
+    defaultFields.forEach(field => {
+      const value = process.env[field];
+      form.getTextField(field).setText(value);
+    });
+
+    form.getTextField("ambiente1").setText(lorem.generateParagraphs(8));
+    form.getTextField("ambiente2").setText(lorem.generateParagraphs(3));
+
     const pdfBytes = await pdfDoc.save();
 
     await writeFile(output, pdfBytes);
-    console.log('PDF Created');
-
-    }catch(err) {
-        console.log(err.message)
-    }
-
+    console.log("PDF Created");
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-createPdf(`${process.env.BASE_FILE}`, `contratoId${process.env.n_contrato}.pdf`)
+createPdf(
+  `${process.env.BASE_FILE}`,
+  `contratoId${process.env.n_contrato}.pdf`
+);
